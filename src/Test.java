6a8aec863b623781d32c8ac655b67cdd0d239819
Maievskyi <h.a.maievskyi@khai.student.edu>
2023-12-08 10:19:18 +0200

    Resolved conflict 3

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,12 @@
 public class Test {
     public static void main(String[] args) {
+
         System.out.println("Java Education");
+
+
+        System.out.println("education&quot");
+        //comment on second string of the func
+        System.out.println("DICT");
 
     }
 }
