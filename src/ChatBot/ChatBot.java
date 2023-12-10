9f004e44dbe8689331c5634f8beebbde5b0569b4
Maievskyi <h.a.maievskyi@khai.student.edu>
2023-12-10 13:34:02 +0200

    ChatBot Stage {2}

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,9 +1,12 @@
 package ChatBot;
-
+import java.util.Scanner;
 public class ChatBot {
     public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! My name is Johny!");
         System.out.println(" I was created in 1803 year before our era");
-
+        System.out.println("Please, remind your name.");
+        String userInput = scanner.nextLine();
+        System.out.println("What a great name you have," + userInput);
     }
 }
