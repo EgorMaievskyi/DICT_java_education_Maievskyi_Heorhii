70239384630d47852eb3318ea4a91922f7f233a1
Maievskyi <h.a.maievskyi@khai.student.edu>
2023-12-10 13:04:25 +0200

    ChatBot Stage {1}

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,9 @@
+package ChatBot;
+
+public class ChatBot {
+    public static void main (String[] args){
+        System.out.println("Hello! My name is Johny!");
+        System.out.println(" I was created in 1803 year before our era");
+
+    }
+}
