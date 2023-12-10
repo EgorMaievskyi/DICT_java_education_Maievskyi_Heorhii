87b6b97dcdbb1139b76e0c08cb146cf4adb0e4af
Maievskyi <h.a.maievskyi@khai.student.edu>
2023-12-10 14:35:18 +0200

    ChatBot Stage {5}

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,7 +1,7 @@
 package ChatBot;
 import java.util.Scanner;
 public class ChatBot {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! My name is Johny!");
         System.out.println(" I was created in 1803 year before our era");
@@ -20,9 +20,28 @@ public class ChatBot {
         System.out.println("Now I will prove to you that I can count to any number you want!");
         System.out.println("Please enter a positive number:");
         int number = scanner.nextInt();
-        for (int i = 1; i<= number; i++){
+        for (int i = 1; i <= number; i++) {
             System.out.println(i + "!");
         }
+        System.out.println("Well, pass one test and you will be free");
+        System.out.println("Let's test your programming knowledge.");
+        System.out.println("Всі змінні в Python являються:");
+        System.out.println("1. Об'єктами");
+        System.out.println("2. Базою");
+        System.out.println("3. Цифрою");
+        System.out.println("4. Строкою");
+        boolean correctAnswer = false;
+        int answer = scanner.nextInt();
+        while (!correctAnswer) {
+            if (answer == 1) {
+                System.out.println("Congratulations, " + userInput + "! Your answer is correct.");
+                correctAnswer = true;
+            } else {
+                System.out.println("Sorry, that's incorrect. Please try again.");
+            }
+            System.out.println("Goodbye, have a nice day!");
 
+        }
     }
 }
+
